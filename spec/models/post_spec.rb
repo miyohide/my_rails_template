744fbceb855a727cab7d665f32ce4aadf152558f
Hidenori Miyoshi <miyohide@gmail.com>
2019-06-09 21:46:28 +0900

    add validation.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "titleが空の場合" do
+    before do
+      @post = Post.create(title: nil, body: "aaa")
+    end
+
+    it "validationがfalseになること" do
+      expect(@post.valid?).to be false
+    end
+  end
+
+  describe "bodyが空の場合" do
+    before do
+      @post = Post.create(title: "aaa", body: nil)
+    end
+
+    it "validationがfalseになること" do
+      expect(@post.valid?).to be false
+    end
+  end
 end
