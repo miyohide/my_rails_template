fa572a451728dca37ccda385e7c6003578949e5a
Hidenori Miyoshi <miyohide@gmail.com>
2019-06-23 16:12:01 +0900

    change redirect paths.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to :users, notice: 'User was successfully created.'
     else
       render :new
     end
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      redirect_to :users, notice: 'User was successfully updated.'
     else
       render :edit
     end
