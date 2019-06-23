78c7cb1bcc6a3cfaa5cf14fb67f79bddaf68f850
Hidenori Miyoshi <miyohide@gmail.com>
2019-06-23 15:43:16 +0900

    define login/logout routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
+  root to: 'posts#index'
+
+  resources :user_sessions, only: [:new, :create, :destroy]
   resources :users
   resources :posts
+
+  get 'login' => 'user_sessions#new', as: :login
+  post 'logout' => 'user_sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
