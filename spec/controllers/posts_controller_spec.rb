99a6df5b3a147d90fe81681bfec8f6c150cda087
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-02 21:51:34 +0900

    add put #update spec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -99,4 +99,39 @@ RSpec.describe PostsController, type: :controller do
       end
     end
   end
+
+  describe "PUT #update" do
+    before do
+      @post = FactoryBot.create(:post)
+      @updated_title = "updated_title1"
+      @updated_body  = "updated_body1"
+    end
+    subject { put :update, params: { id: @post.id, post: { title: @updated_title, body: @updated_body } }}
+
+    context "ログインしていないとき" do
+      it "ログイン画面にリダイレクトすること" do
+        expect(subject).to redirect_to("/login")
+      end
+    end
+
+    context "ログインしているとき" do
+      before do
+        login_user(FactoryBot.create(:user))
+      end
+
+      it "ログイン画面にリダイレクトせずにPostの詳細画面にリダイレクトすること" do
+        expect(subject).to redirect_to("/posts/#{@post.id}")
+      end
+
+      it "Postが増えていないこと" do
+        expect{ subject }.to change{ Post.count }.by(0)
+      end
+
+      it "Postの値が更新されていること" do
+        subject
+        expect(Post.find(@post.id).title).to eq(@updated_title)
+        expect(Post.find(@post.id).body).to eq(@updated_body)
+      end
+    end
+  end
 end
