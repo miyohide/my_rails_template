a590532c04b2992cac645bce23b8c1eb61b7ce89
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-03 21:14:09 +0900

    fix user_sessions#new spec.

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe UserSessionsController, type: :controller do
 
   describe "GET #new" do
-    it "returns http success" do
+    it "ログイン画面にアクセスすること" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
     end
   end
 
