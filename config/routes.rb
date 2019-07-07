089f906a4aeaa0b1897f0f0369e3b0c543e1ff66
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-07 17:16:09 +0900

    fix http method.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :posts
 
   get 'login' => 'user_sessions#new', as: :login
-  post 'logout' => 'user_sessions#destroy', as: :logout
+  delete 'logout' => 'user_sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
