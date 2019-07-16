6a7fe1ebefb3c75b64ca21e425f501f4926a8eb3
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-16 17:06:48 +0900

    create first request spec.

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
-    xit "works! (now write some real specs)" do
-      get posts_path
-      expect(response).to have_http_status(200)
+    subject { get posts_path }
+
+    context 'when user does not login' do
+      it 'redirect to login page' do
+        subject
+        expect(response).to redirect_to('/login')
+      end
     end
   end
 end
