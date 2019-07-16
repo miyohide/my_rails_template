28a83229c8c0ec6316895396eadedb1d6efa914a
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-16 17:29:36 +0900

    add GET /posts spec when user login.

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,5 +10,21 @@ RSpec.describe "Posts", type: :request do
         expect(response).to redirect_to('/login')
       end
     end
+
+    context 'when user login' do
+      before do
+        @posts = FactoryBot.create_list(:post, 3)
+        @user = FactoryBot.create(:user)
+        # Request SpecではSorceryのlogin_userメソッドがうまく動かないので、
+        # 直接/user_sessionsにログイン情報を渡してログイン処理を実施することにした。
+        # 参考: https://github.com/NoamB/sorcery/issues/775
+        post '/user_sessions', params: FactoryBot.attributes_for(:user)
+        subject
+      end
+
+      it 'success to request' do
+        expect(response.status).to eq 200
+      end
+    end
   end
 end
