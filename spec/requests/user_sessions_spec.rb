2ff64acba7c1dc3a5e42f564c2177280752fa5bd
Hidenori Miyoshi <miyohide@gmail.com>
2019-09-21 17:10:40 +0900

    add post /user_sessions request spec.

--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe "UserSessions", type: :request do
       expect(response.body).to include("Login")
     end
   end
+
+  describe "POST /user_sessions" do
+    context "登録されているユーザ情報を与えた場合" do
+      before do
+        @user_param = FactoryBot.attributes_for(:user)
+        User.create(@user_param)
+      end
+
+      it "ログインに成功してリダイレクトすること" do
+        post "/user_sessions", params: @user_param
+        expect(response).to redirect_to(posts_path)
+      end
+    end
+
+    context "登録されていないユーザ情報を与えた場合" do
+      before do
+        @user_param = FactoryBot.attributes_for(:user)
+      end
+
+      it "再度ログイン画面を表示すること" do
+        post "/user_sessions", params: @user_param
+        expect(response.body).to include("Login")
+      end
+    end
+  end
 end
