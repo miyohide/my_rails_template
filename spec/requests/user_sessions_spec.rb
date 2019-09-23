a14a0536c6831e0c801485d80c6e6476bb2c7f6a
Hidenori Miyoshi <miyohide@gmail.com>
2019-09-23 17:23:44 +0900

    delete user_sessions#new path. use login.

--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "UserSessions", type: :request do
   describe "GET /user_sessions/new" do
     it "成功すること" do
-      get "/user_sessions/new"
+      get "/login"
       expect(response).to have_http_status(:success)
     end
 
     it "ログイン画面にアクセスすること" do
-      get "/user_sessions/new"
+      get "/login"
       expect(response.body).to include("Login")
     end
   end
