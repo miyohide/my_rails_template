2197b0a8d4c2ffb32f3cfd104bd2114f0be0585a
Hidenori Miyoshi <miyohide@gmail.com>
2019-10-01 21:21:54 +0900

    fix Layout/IndentationConsistency

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,13 +47,13 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
