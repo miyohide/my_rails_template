b635c62926ff7e1784ff8f75c6bc0cc5a2dca664
Hidenori Miyoshi <miyohide@gmail.com>
2019-10-21 17:22:39 +0900

    画面ごとにJSを返すようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  # 画面ごとのJavaScriptのパスを返す
+  def js_path
+    path = "#{controller_path}/#{action_name}.js"
+    if js_file_exist?(path)
+      path
+    else
+      "default.js"
+    end
+  end
+
+  def js_file_exist?(path)
+    manifest = JSON.parse(File.read("public/packs/manifest.json"))
+    manifest.key?(path)
+  end
 end
