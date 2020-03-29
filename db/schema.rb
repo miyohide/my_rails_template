47f018d13bf22fe3b2e07cb142c1c66e3f48e912
Hidenori Miyoshi <miyohide@gmail.com>
2020-03-29 18:37:04 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_08_045825) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
