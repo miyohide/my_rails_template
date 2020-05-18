a3fed634c16cd997e1c447eb0c096b8a4e4a28e9
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-18 20:58:28 +0900

    add posts#show tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,4 +5,16 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     get posts_url
     assert_response :success
   end
+
+  test "should get show" do
+    post = Post.create(title: "t", body: "b")
+    get post_url(post.id)
+    assert_response :success
+  end
+
+  test "shouldn't get show when id doesn't exist" do
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get post_url(1234)
+    end
+  end
 end
