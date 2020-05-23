839a3fdbf71f0e0601a7c9e4b47779c2c7ca622d
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-23 21:30:25 +0900

    add comments.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,8 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
+  # disable parallel test because simplecov issue.
+  # see. https://github.com/colszowka/simplecov/issues/718
   # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
