e88f0ef19c24cc48ec9d1758ef0185921719c79a
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-25 19:09:14 +0900

    add a test that could not generate post.

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -43,6 +43,12 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to post_path(Post.last)
   end
 
+  test "should not create post" do
+    post posts_url, params: { post: { title: 't' * 201, body: 'b' } }
+
+    assert_match "Title is too long", @response.body
+  end
+
   test "should update post" do
     post = Post.create(title: "t", body: "b")
     patch post_url(post.id), params: { post: { title: 't1', body: 'b1' } }
