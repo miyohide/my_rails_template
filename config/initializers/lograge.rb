1640763439e6a57e33cc4bcf00a66c22a577e153
Hidenori Miyoshi <miyohide@gmail.com>
2020-06-04 21:05:36 +0900

    change lograge formatter.

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.lograge.enabled = true
+  config.lograge.formatter = Lograge::Formatters::Json.new
 
   # add time to lograge
   config.lograge.custom_options = lambda do |e|
