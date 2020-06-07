03a9dd0472eaf3beec984d6e1f81eac912957143
Hidenori Miyoshi <miyohide@gmail.com>
2020-06-07 18:19:03 +0900

    fix deprecation warning.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,5 +62,5 @@ Rails.application.configure do
   config.session_store :redis_store,
     servers: [ENV['REDIS_URL']],
     expire_after: 90.minutes,
-    key: "_#{Rails.application.class.parent_name.downcase}_session"
+    key: "_#{Rails.application.class.module_parent_name.downcase}_session"
 end
