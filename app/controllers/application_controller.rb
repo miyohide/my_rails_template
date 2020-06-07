17720cdc71e1957ea008e6c3bce50eb86a2259af
Hidenori Miyoshi <miyohide@gmail.com>
2020-06-07 17:46:31 +0900

    add a procesure that set session data.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :require_login
+  before_action :require_login, :set_session_info
 
   private
+  # セッションにダミーデータを設定する
+  def set_session_info
+    session[:session_info] = "foo bar"
+  end
+
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
   end
