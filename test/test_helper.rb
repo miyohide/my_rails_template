08d2aee1bd24283e92642b17268c4247e0133bc9
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-11 14:56:06 +0900

    add config to minitest-reporters.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 SimpleCov.start 'rails'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::JUnitReporter.new
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
