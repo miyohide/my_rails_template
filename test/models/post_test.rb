52f688dab14ce492528501920b48fa7a33177268
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-11 15:58:53 +0900

    fix test.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class PostTest < ActiveSupport::TestCase
   test "should not save post without title" do
     post = Post.new(body: 'body')
-    # assert_not post.save
-    assert post.save
+    assert_not post.save
   end
 
   test "should not save post when tile is very long" do
