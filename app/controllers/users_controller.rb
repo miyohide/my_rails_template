c5a45ab9a4cdc7646cb88a5e48baaa02d6d27bab
Hidenori Miyoshi <miyohide@gmail.com>
2020-09-11 19:49:27 +0900

    Fix an issue where users could not be created.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email, :crypted_password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
