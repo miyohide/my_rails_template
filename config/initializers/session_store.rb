b4cd243afa11819ed928327166d3894a3fdaf0ec
Hidenori Miyoshi <miyohide@gmail.com>
2020-09-15 22:04:58 +0900

    add redis password.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
-  config.session_store :redis_store,
-  servers: [ENV['REDIS_URL']],
-  expire_after: 90.minutes,
+  config.session_store :redis_store, expire_after: 90.minutes, servers: {
+    host: ENV['REDIS_HOST'],
+    port: ENV['REDIS_PORT'],
+    db: ENV['REDIS_DB'],
+    password: ENV['REDIS_PASSWORD'],
+    namespace: 'session',
+  },
   key: "_#{Rails.application.class.module_parent_name.downcase}_session"
 end
