267f281ba6d4cc16146b77ae05cb998d76b1765c
Hidenori Miyoshi <miyohide@gmail.com>
2021-06-23 21:03:30 +0900

    add an option that not allow null

--- db/migrate/20210623115620_create_todos.rb
+++ db/migrate/20210623115620_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
 
       t.timestamps
     end
