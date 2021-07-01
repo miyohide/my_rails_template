c60c95b2a887e2e0dcd99de2bc9180b6841c1063
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-01 21:28:48 +0900

    add validation test.

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "タイトルに何も入力されていないとき、バリデーションエラー" do
+    todo = Todo.new(body: "test body")
+    assert_not todo.save
+  end
+
+  test "本文に何も入力されていないとき、バリデーションエラー" do
+    todo = Todo.new(title: "test title")
+    assert_not todo.save
+  end
+
+  test "タイトルと本文に入力されているとき、保存できる" do
+    todo = Todo.new(title: "test title", body: "test body")
+    assert todo.save
+  end
 end
