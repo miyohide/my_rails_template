01fa626f92802f2a5130c43f79f4462f4a9691b4
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-02 21:16:44 +0900

    add validation for body field

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ApplicationRecord
   validates :title, presence: true, length: { maximum: 200 }
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 5000 }
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -16,12 +16,12 @@ class TodoTest < ActiveSupport::TestCase
     assert todo.save
   end
 
-  test "長いタイトル - 1文字入力すると、保存できる" do
+  test "最大文字数分タイトルを入力すると、保存できる" do
     todo = Todo.new(title: "a" * 200, body: "test body")
     assert todo.save
   end
 
-  test "長いタイトルを入力すると、バリデーションエラー" do
+  test "最大文字数分 + 1タイトルを入力すると、バリデーションエラー" do
     todo = Todo.new(title: "a" * 201, body: "test body")
     assert_not todo.save
   end
@@ -33,6 +33,16 @@ class TodoTest < ActiveSupport::TestCase
 
   test "空白文字タイトルを入力すると、バリデーションエラー" do
     todo = Todo.new(title: "   ", body: "test body")
+    assert_not todo.save
+  end
+
+  test "最大文字数分本文を入力すると、保存できる" do
+    todo = Todo.new(title: "test title", body: "b" * 5000)
+    assert todo.save
+  end
+
+  test "最大文字数分 + 1本文を入力すると、バリデーションエラー" do
+    todo = Todo.new(title: "test title", body: "b" * 5001)
     assert_not todo.save
   end
 
