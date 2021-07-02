3b4a7a655fed9b242062912caf9a76efe3df450e
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-02 21:20:47 +0900

    fix fixture

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @todo = todos(:one)
+    @todo = todos(:valid)
   end
 
   test "should get index" do
