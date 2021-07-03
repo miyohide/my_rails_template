77ad8aec3b8c5b9126c89e93a74dbfb4bf8f51fc
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-03 20:57:54 +0900

    setup for simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
+require "simplecov"
+SimpleCov.start "rails"
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # workersの値を1にしているのはカバレッジを取得するため。複数個の場合はうまく動かない
+  parallelize(workers: 1)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
