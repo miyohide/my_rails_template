96a5c8e7e1525ea7c894e911bccecbfc6a4eafc7
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-10 21:08:52 +0900

    fix classAndModuleChildren

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,13 +5,15 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  # workersの値を1にしているのはカバレッジを取得するため。複数個の場合はうまく動かない
-  parallelize(workers: 1)
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    # workersの値を1にしているのはカバレッジを取得するため。複数個の場合はうまく動かない
+    parallelize(workers: 1)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
+  end
 end
