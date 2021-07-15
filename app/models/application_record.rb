1db8f819a797cb0c50340c6edcba344c2f816c4d
Hidenori Miyoshi <miyohide@gmail.com>
2021-07-15 21:28:48 +0900

    Add comments for model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
+# ApplicationRecord は Rails のモデルのベースとなるクラス
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,4 @@
+# Todo モデルは、 やること（todo）の情報を保持するクラス
 class Todo < ApplicationRecord
   validates :title, presence: true, length: { maximum: 200 }
   validates :body, presence: true, length: { maximum: 5000 }
