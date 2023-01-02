959fdd8e02ea80732e269c5d2fe837c5bd38a35f
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-02 18:30:21 +0900

    update comments

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
 // BootstrapのCSSやJavaScriptをインポートする
-// see. https://getbootstrap.com/docs/5.0/getting-started/webpack/
 import 'bootstrap/dist/css/bootstrap.min.css';
+// BootstrapのJavaScript側の機能を読み込む
 import 'bootstrap';
 import '@hotwired/turbo-rails';
 import './controllers';
