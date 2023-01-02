d836bfa3e145b1c92edae4370c0c2c1ebe311ab8
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-02 16:45:26 +0900

    import bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Entry point for the build script in your package.json
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import '@hotwired/turbo-rails';
 import './controllers';
