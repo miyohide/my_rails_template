4fc2b7357424b889611dcee86381886b5b9b74ce
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-08 20:48:05 +0900

    fix rubocop cops.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length: { maximum: 50 }
-    validates :body, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true
 end
