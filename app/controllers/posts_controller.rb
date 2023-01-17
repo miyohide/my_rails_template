00509cea990ff4a58a3b1d474e630a5c794168e6
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-17 20:12:49 +0900

    add yard comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,23 +2,23 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
-  # GET /posts or /posts.json
+  # GET /posts もしくは GET /posts.json のリクエストが発生した時の処理
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
+  # GET /posts/:id もしくは GET /posts/:id.json のリクエストが発生した時の処理
   def show; end
 
-  # GET /posts/new
+  # GET /posts/new のリクエストが発生した時の処理
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
+  # GET /posts/:id/edit のリクエストが発生した時の処理
   def edit; end
 
-  # POST /posts or /posts.json
+  # POST /posts もしくは POST /posts.json のリクエストが発生した時の処理
   def create
     @post = Post.new(post_params)
 
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
+  # PATCH/PUT /posts/:id もしくは PATCH /posts/:id.json のリクエストが発生した時の処理
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -46,7 +46,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
+  # DELETE /posts/:id もしくは /posts/:id.json のリクエストが発生した時の処理
   def destroy
     @post.destroy
 
@@ -58,12 +58,13 @@ class PostsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+  # URL内に:idが含まれている場合、それをキーにしてPostを検索し、@post に格納する
+  # @return [Post] :idを主キーとするPostのインスタンス
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
+  # 外部から与えられたリクエストパラメータのうち、許可するもののリストを定義する
   def post_params
     params.require(:post).permit(:title, :body)
   end
