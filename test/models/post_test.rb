7353a89673fa8485f314d1e3e21fa30877d57ad1
Hidenori Miyoshi <miyohide@gmail.com>
2024-01-20 20:03:10 +0900

    add validation and create tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "titleの入力がないとき、validationに失敗すること" do
+    p = Post.new(body: "this is body")
+    assert !p.valid?
+  end
+
+  test "titleの入力が51文字入力されているとき、validationに失敗すること" do
+    p = Post.new(title: "a" * 51, body: "body")
+    assert !p.valid?
+  end
+
+  test "bodyの入力がないとき、validationに失敗すること" do
+    p = Post.new(title: "this is title")
+    assert !p.valid?
+  end
 end
