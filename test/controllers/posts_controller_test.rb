b06549518013876b79e1c7e4db5c518de2d68768
Hidenori Miyoshi <miyohide@gmail.com>
2024-01-22 20:49:08 +0900

    fixtureの名前を修正

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @post = posts(:one)
+    @post = posts(:normal_post)
   end
 
   test "should get index" do
