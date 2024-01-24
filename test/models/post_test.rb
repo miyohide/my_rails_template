27b9d82d7c939ac73bbaaee06b83bc4e238aa329
Hidenori Miyoshi <miyohide@gmail.com>
2024-01-24 20:13:57 +0900

    fix rubocop warning. use assert_not

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,27 +3,27 @@ require "test_helper"
 class PostTest < ActiveSupport::TestCase
   test "titleの入力がないとき、validationに失敗すること" do
     p = Post.new(body: "this is body")
-    assert !p.valid?
+    assert_not p.valid?
   end
 
   test "titleの入力が空文字のとき、validationに失敗すること" do
     p = Post.new(title: "", body: "this is body")
-    assert !p.valid?
+    assert_not p.valid?
   end
 
   test "titleの入力が51文字入力されているとき、validationに失敗すること" do
     p = Post.new(title: "a" * 51, body: "body")
-    assert !p.valid?
+    assert_not p.valid?
   end
 
   test "bodyの入力がないとき、validationに失敗すること" do
     p = Post.new(title: "this is title")
-    assert !p.valid?
+    assert_not p.valid?
   end
 
   test "bodyの入力が空文字のとき、validationに失敗すること" do
     p = Post.new(title: "this is title", body: "")
-    assert !p.valid?
+    assert_not p.valid?
   end
 
   test "titleの入力が50文字以下で、bodyの入力があるとき、Postが作成できること" do
